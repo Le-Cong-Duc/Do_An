46d43487c3d988dbb22f7018576f7e4cb61e791f
Le-Cong-Duc <congduc1801@gmail.com>
2024-11-19 14:12:09 +0700

    billl

--- Do_An/Public/js/view.js
+++ Do_An/Public/js/view.js
@@ -14,22 +14,3 @@ function changeQuantity(change) {
 }
 document.getElementById('quantity').addEventListener('input', updateHiddenQuantity);
 
-
-function approveOrder(billId) {
-    if (confirm("Bạn có chắc chắn muốn duyệt đơn hàng này?")) {
-        $.ajax({
-            url: 'index.php?action=update_bill', // URL xử lý yêu cầu
-            method: 'POST', // Phương thức gửi yêu cầu
-            data: { bill_id: billId }, // Dữ liệu gửi đi
-            success: function(response) {
-                alert("Đơn hàng đã được duyệt thành công!");
-                // Cập nhật trạng thái của đơn hàng mà không tải lại trang
-                document.querySelector(`#status_${billId}`).innerText = "Đã Duyệt";
-            },
-            error: function() {
-                alert("Có lỗi xảy ra khi duyệt đơn hàng!");
-            }
-        });
-    }
-}
-
